d061c3fbabf3a676650a5861edf5bec627666aad
eateat8857 <110526947+eateat8857@users.noreply.github.com>
2023-02-17 20:26:11 +0800

    Update and rename PHP_homework to week1.php

---
+++ week1.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <meata charset="UTF-8">
+    <title>A1101255懿慈的家</title>
+  </head>
+  <body text=#BF8F8B bgcolor=#CAB8B2>
+    <?php echo "Hello PHP\n";
+    ?>
+  </body>
+  </html>>
